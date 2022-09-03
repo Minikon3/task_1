541d0048ea0da73ed0cce8969cf46cd08466f942
Minikon3 <112776259+Minikon3@users.noreply.github.com>
2022-09-03 21:14:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr1=new int[]{1,2,3,4,5,6,7,8,9,10};
+        int sum=0;
+        for (int i=0;i< arr1.length;i++){
+            sum=sum+arr1[i];
+        }
+        System.out.println(sum);
+    }
+}
